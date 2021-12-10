c8ad9a8c7b166998411e06e322221c58567bc6b3
mickael j <joseph.mickk@gmail.com>
2021-12-10 14:36:05 +0100

    init

---
+++ src/main/java/com/medicreen/patientnote/PatientnoteApplication.java
@@ -0,0 +1,13 @@
+package com.medicreen.patientnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicreen/patientnote/PatientnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicreen.patientnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
